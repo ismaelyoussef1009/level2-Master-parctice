b2e537721cc2b8274048503c55cfb0929cf62ee6
ismaelyoussef1009 <ismaelyoussef1009>
2022-12-06 12:33:25 +0100

    text 1

---
+++ index.js
@@ -0,0 +1,13 @@
+
+
+// Section 2 question 3
+function sumArray(number) {
+    let sum = 0;
+    for (let i = 0; i < number.length; i++) {
+      sum += number[i];
+    }
+    
+    return sum;
+  }
+  console.log(sumArray([2,7,3,6,4]));
+
